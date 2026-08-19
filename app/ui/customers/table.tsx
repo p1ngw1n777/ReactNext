f60f6e761b37dc0c2c3ba8a8c38231316b7bac5f
pingwin <laikerfeed00@gmail.com>
2026-08-19 16:14:01 +0200

    fixed: alias @

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import Search from '@/app/ui/search';
+import { lusitana } from '@app/app/ui/fonts';
+import Search from '@app/app/ui/search';
 import {
   CustomersTableType,
   FormattedCustomersTable,
-} from '@/app/lib/definitions';
+} from '@app/app/lib/definitions';
 
 export default async function CustomersTable({
   customers,
