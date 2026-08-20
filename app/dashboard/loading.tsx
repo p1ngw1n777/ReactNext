fc839932cbaac2cd7ea0fcb56296c59ee6dcfdca
pingwin <laikerfeed00@gmail.com>
2026-08-20 12:06:46 +0200

    finished: Chapter 9, added Suspence

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,5 @@
+import DashboardSkeleton from '@app/app/ui/skeletons';
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -2,6 +2,7 @@ import { generateYAxis } from '@app/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@app/app/ui/fonts';
 import { Revenue } from '@app/app/lib/definitions';
+import { fetchRevenue } from '@app/app/lib/data';
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -9,11 +10,9 @@ import { Revenue } from '@app/app/lib/definitions';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+export default async function RevenueChart() { // Make component async, remove the props
+  const revenue = await fetchRevenue(); // Fetch data inside the component
+ 
   const chartHeight = 350;
   //NOTE: Uncomment this code in Chapter 7
 
